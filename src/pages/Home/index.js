fa5e346cc796cad4df96f8ef2c8f90fb24381edf
John Smith <ferisakuragi@gmail.com>
2021-01-30 19:57:10 +0700

    membuat daftar produk
    Pagination produk | loading data product

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -3,15 +3,18 @@ import {
     SideNav,
     LayoutSidebar,
     Responsive,
-    CardProduct
+    CardProduct,
+    Pagination,
 } from 'upkit'
 
 import { useDispatch, useSelector } from 'react-redux';
+// import BounceLoader from 'react-spinners/BounceLoader'
+import HashLoader from 'react-spinners/HashLoader'
 
 import menus from './menus'
 import TopBar from '../../components/TopBar'
 import { config } from '../../config'
-import { fetchProducts } from '../../features/Products/actions';
+import { fetchProducts, goToNextPage, goToPrevPage, setPage } from '../../features/Products/actions';
 
 
 
@@ -22,7 +25,7 @@ export default function Home() {
 
     React.useEffect(() => {
         dispatch(fetchProducts())
-    }, [dispatch])
+    }, [dispatch, products.currentPage])
 
     return (
         <div>
@@ -38,9 +41,18 @@ export default function Home() {
                     <div className="md:flex md:flex-row-reverse w-full mr-5 h-full min-h-screen">
                         <div className="w-full md:w-3/4 pl-5 pb-10">
                             <TopBar />
+
+                            {products.status === 'process' && !products.data.length ?
+                                <div className="flex justify-center text-center">
+                                    <HashLoader
+                                        color="#3182CE"
+                                    />
+                                </div>
+                                : null}
+
                             <Responsive desktop={3} items="stretch">
                                 {products.data.map((product, index) => {
-                                    return <div key={index} className="p-2">
+                                    return <div key={index} className="p-2 mt-5">
                                         <CardProduct
                                             color="blue"
                                             title={product.name}
@@ -51,6 +63,18 @@ export default function Home() {
                                     </div>
                                 })}
                             </Responsive>
+                            <div className="text-center mt-10">
+                                <Pagination
+                                    color="blue"
+                                    totalItems={products.totalItems}
+                                    page={products.currentPage}
+                                    perPage={products.perPage}
+                                    onChange={page => dispatch(setPage(page))}
+                                    onNext={_ => dispatch(goToNextPage())}
+                                    onPrev={_ => dispatch(goToPrevPage())}
+                                />
+                            </div>
+
                         </div>
                         <div className="w-full md:w-1/4 h-full shadow-lg border-r border-white bg-gray-100">
                             Keranjang belanja di sini
